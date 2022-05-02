d83e30eb870ba7e255b98f92419ef1f6d45d61e7
Noam Cohen <noam60@gmail.com>
2022-05-02 19:59:54 +0300

    added models and updated them

---
+++ Models/Chat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models {
+    public class Chat {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Please enter the participants.")]
+        public List<string> Participants { get; set; }
+
+        [Required(ErrorMessage = "Please enter the messages.")]
+        public List<Message> Messages { get; set; }
+    }
+}
---
+++ Models/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models {
+    public class Message {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(10)]
+        public string Type { get; set; }
+        
+        [Required(ErrorMessage ="Please enter the message text")]
+        public string Text { get; set; }
+        
+        [Required(ErrorMessage ="Please enter the sender username")]
+        public string SenderUsername { get; set; }
+        
+        [Required(ErrorMessage ="Please enter when the message was written in")]
+        public DateTime WrittenIn { get; set; }
+    }
+}
