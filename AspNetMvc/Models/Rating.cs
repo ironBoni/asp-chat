fb8234809d13b7cbcf8c6168438a4375ae85afcd
Noam Cohen <noam60@gmail.com>
2022-05-03 18:40:00 +0300

    added rating controller and model and service

--- AspNetMvc/Models/Rating.cs
+++ AspNetMvc/Models/Rating.cs
@@ -15,5 +15,13 @@ namespace AspNetMvc.Models
         [MaxLength(200, ErrorMessage = "Exceeded max length! 200 chars requried")]
         public string Feedback { get; set; }
 
+        public Rating(int id, int rate, string name, string feedback)
+        {
+            ID = id;
+            Rate = rate;
+            Name = name;   
+            Feedback = feedback;
+        }
+
     }
 }
---
+++ AspNetMvc/Models/RatingService.cs
@@ -0,0 +1,51 @@
+﻿using Models;
+
+namespace AspNetMvc.Models {
+    public class RatingService : IDataService<Rating, int> {
+
+        private static List<Rating> ratings = new List<Rating>()
+        {
+            new Rating(1, 5, "dvir", "Awesome website!"),
+            new Rating(1, 5, "hadar", "Very good website"),
+            new Rating(1, 4, "dan", "I liked this")
+        };
+
+        public bool Create(Rating entity)
+        {
+            ratings.Add(entity);
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            var rating = ratings.Find(rating => rating.ID == id);
+            if(rating == null) 
+                return false;
+
+            ratings.Remove(rating);
+            return true;
+        }
+
+        public List<Rating> GetAll()
+        {
+            return ratings;
+        }
+
+        public Rating GetById(int id)
+        {
+            return ratings.Find(rating => rating.ID == id);
+        }
+
+        public bool Update(Rating entity)
+        {
+            var rating = ratings.Find(rating => rating.ID == entity.ID);
+
+            if (rating == null)
+                return false;
+
+            rating.Feedback = entity.Feedback;
+            rating.Rate = entity.Rate;
+            return true;
+        }
+    }
+}
