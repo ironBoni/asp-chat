190cf6e78381380836c35c9bf85c77b3c9b6b61f
hadarpinto <hadarpinto@gmail.com>
2022-05-07 13:29:44 +0300

    rating styled

--- AspNetMvc/Program.cs
+++ AspNetMvc/Program.cs
@@ -28,10 +28,9 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Ratings}/{action=Index}/{id?}");
 
-app.UseEndpoints(endpoints =>
+/*app.UseEndpoints(endpoints =>
 {
     endpoints.MapHub<MyHub>("/myHub");
-});
-*/
+});*/
 app.Run();
 
--- AspNetMvc/wwwroot/js/site.js
+++ AspNetMvc/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+/*
+ revealing starrs by percent
+*/
+
+$(document).ready(function () {
+    // Gets the span width of the filled-ratings span
+    // this will be the same for each rating
+    var star_rating_width = $('.fill-ratings span').width();
+    // Sets the container of the ratings to span width
+    // thus the percentages in mobile will never be wrong
+    $('.star-ratings').width(star_rating_width);
+});
