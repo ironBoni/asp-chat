24871a1266802e32f5b1e2fdf5cd109f5ea2a06c
Noam Cohen <noam60@gmail.com>
2022-05-07 23:52:14 +0300

    react in server noamc

---
+++ Main/Models/Contacts/MessageResponse.cs
@@ -0,0 +1,28 @@
+﻿namespace AspWebApi.Models.Contacts {
+    [Serializable]
+    public class MessageResponse {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+        public bool Sent { get; set; }
+
+        public string Type { get; set; }
+        public string SenderUsername { get; set; }
+        
+        public string FileName { get; set; }
+        public MessageResponse(int id, string content, DateTime created, bool sent, string senderUsername)
+        {
+            Id = id;
+            Content = content;
+            Created = created;
+            Sent = sent;
+            Type = "text";
+            SenderUsername = senderUsername;
+            FileName = "";
+        }
+
+        public MessageResponse()
+        {
+        }
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,33 @@
+using Models.DataServices;
+using Models.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(p => p.AddPolicy("cors", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("cors");
+app.UseAuthorization();
+
+app.MapControllers();
+
+Current.Username = "noam";
+UserService.SetContactsForEveryUser();
+
+app.Run();
