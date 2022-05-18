90a39ec7ff566ef385b6afbc47e59b527af4e16c
Noam Cohen <noam60@gmail.com>
2022-05-18 19:23:12 +0300

    If there were errors in register, then register is valid then it does it immediately

---
+++ Models/Models/UsersList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models {
+    public class UsersList {
+        public List<string> FulLList { get; set; }
+
+        public UsersList(List<string> fullList)
+        {
+            FulLList = fullList;   
+        }
+    }
+}
