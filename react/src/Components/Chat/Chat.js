719aee96d1ada6ee4275df06bc6e5d5969f29fc1
Noam Cohen <noam60@gmail.com>
2022-05-18 10:11:28 +0300

    fix error nav

--- react/src/Components/Chat/Chat.js
+++ react/src/Components/Chat/Chat.js
@@ -21,7 +21,7 @@ const Chat = (props) => {
         <>
             <Navbar bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand className="nav" onClick={handleClick}> <img class="logo-nav" src="/images/webIcon.png" /></Navbar.Brand>
+                    <Navbar.Brand className="nav" onClick={handleClick}> <img className="logo-nav" src="/images/webIcon.png" /></Navbar.Brand>
                     <Nav className="me-auto">
                     <Nav.Link className="nav" href="http://localhost:5266/Ratings/">Ratings</Nav.Link>
                     <Nav.Link className="nav" onClick={handleClick}>Logout</Nav.Link>
